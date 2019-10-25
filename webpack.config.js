418ab1715049faeca2d78a090e74a795d6f5e3b2
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-25 13:27:44 +0530

    use loaders to support bundling of scss files

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
 
 module.exports = {
     mode: 'development',
@@ -32,6 +9,7 @@ module.exports = {
         background: './src/scripts/background.js',
         popup: './src/scripts/popup.js',
         options: './src/scripts/options.js',
+        styles: ['./src/styles/popup.scss', './src/styles/options.scss'],
     },
 
     output: {
@@ -61,24 +39,32 @@ module.exports = {
                     ],
                 },
             },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].css',
+                            context: './src/styles/',
+                            outputPath: 'css/',
+                        },
+                    },
+                    'extract-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    'resolve-url-loader',
+                    'sass-loader',
+                ],
+            },
         ],
     },
 
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendors: {
-                    priority: -10,
-                    test: /[\\/]node_modules[\\/]/,
-                },
-            },
-
-            chunks: 'async',
-            minChunks: 1,
-            minSize: 30000,
-            name: true,
-        },
-    },
+    optimization: {},
 
     devServer: {
         open: true,
