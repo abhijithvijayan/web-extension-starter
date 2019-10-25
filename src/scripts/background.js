dd18c91b214c6e8b0c27c05ad11099f3b668cd8c
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-25 16:29:59 +0530

    send sample message on popup click to background.js

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,1 +1,14 @@
-console.log('Hello World from background main file!');
+import browser from 'webextension-polyfill';
+
+browser.runtime.onInstalled.addListener(() => {
+    // eslint-disable-next-line no-console
+    console.log('onInstalled....');
+});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // Do something with the message!
+    alert(request.url);
+
+    // And respond back to the sender.
+    return Promise.resolve('got your message, thanks!');
+});
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,1 +1,18 @@
-console.log('Hello World from popup main file!');
+import browser from 'webextension-polyfill';
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const tabs = await browser.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+    });
+
+    const url = tabs.length && tabs[0].url;
+
+    const response = await browser.runtime.sendMessage({
+        msg: 'hello',
+        url,
+    });
+
+    // eslint-disable-next-line no-console
+    console.log(response);
+});
