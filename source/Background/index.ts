db8e830161ba5be91b58125c9d34dd4749a853cd
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2020-04-28 23:52:23 +0530

    chore: add unicorn.log for logging with 🦄

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -1,5 +1,6 @@
+import 'unicorn.log';
 import {browser} from 'webextension-polyfill-ts';
 
 browser.runtime.onInstalled.addListener((): void => {
-  console.log('extension installed');
+  console.unicorn('extension installed');
 });
